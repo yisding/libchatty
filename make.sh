36915e52b3f15e7d93d1d7b7a48d356e8b4ec980
Yi Ding <yi.s.ding@gmail.com>
2024-07-09 00:08:22 -0700

    Ofast doesn't seem to do much

--- make.sh
+++ make.sh
@@ -4,9 +4,9 @@
 #export CPPFLAGS="-I/opt/homebrew/opt/curl/include"
 #export PKG_CONFIG_PATH="/opt/homebrew/opt/curl/lib/pkgconfig"
 
-clang -c chatty.c cJSON.c
+clang -Ofast -c chatty.c cJSON.c
 ar rcs libchatty.a chatty.o cJSON.o
-clang -L. -lcurl -lchatty main.c -o chatty
+clang -Ofast -L. -lcurl -lchatty main.c -o chatty
 
 # If you get an error it's because you forgot to brew install curl
 # Or maybe you're using a different laptop.
